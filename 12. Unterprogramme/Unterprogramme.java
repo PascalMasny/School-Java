95ced493b65234dca0baacc6faecc3e297c98aab
Passi <62151638+PascalMasny@users.noreply.github.com>
2021-07-07 21:27:50 +0200

    Initial Commit

---
+++ 12. Unterprogramme/Unterprogramme.java
@@ -0,0 +1,31 @@
+public class Unterprogramme
+{
+
+    //Funktion ohne Parameter Übergabe
+    static void myFunktion1(){
+        System.out.println("Ausführen der Methode");
+    }
+
+    //Funktion mit Parameter Übergabe
+    static void myFunktion2(String name){
+        System.out.println(name);
+    }
+
+
+
+    public static void main (String[] args){
+        
+        //Funktion ohne Parameter
+        myFunktion1();
+
+        //Funktion mit Parameter
+        myFunktion2("Grüße aus der Funktion");
+
+        //
+
+
+
+
+    }
+    
+}
