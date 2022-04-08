e7e87a94e7e77c30ad7d65b56dd3f3ef8cc3e5e0
Pascal Masny <pascal.masny@protonmail.com>
2022-04-08 12:46:17 +0200

    fix 25. Palindrom

--- 25. Palindrom/MeinStack.java
+++ 25. Palindrom/MeinStack.java
@@ -1,9 +1,9 @@
 public class MeinStack extends Palindromtester{
     
-    public char[] stapel;
-    public ibt tos = 0; //tos = top of stack
+    private char[] stapel = new char[100];
+    private int tos = 0; //tos = top of stack
 
-    public  cahr pop(){
+    public char pop(){
         char ch = stapel[tos - 1];
         stapel[tos - 1] = '\0';
         tos--;
@@ -14,9 +14,5 @@ public class MeinStack extends Palindromtester{
         stapel[tos] = c;
         tos++;
     }
-    
-
-    
-
     
 }
--- 25. Palindrom/Palindromtester.java
+++ 25. Palindrom/Palindromtester.java
@@ -6,10 +6,23 @@ public class Palindromtester {
 	public static void main(String[] args) {
 		
 		MeinStack mystack = new MeinStack();
-        String input = "ABBA"; //JOptionPane.showInputDialog(null, "Bitte ein String eingeben");
+        String input = JOptionPane.showInputDialog(null, "Bitte ein String eingeben"); //BSP: ABBA
 
+		//push in to stack
 		for (int i = 0; i < input.length(); i++) {
 			mystack.push(input.charAt(i));
+		}
+		
+		//ckeck if Palindrom
+		for (int i = 0; i < input.length(); i++) {
+			if (mystack.pop() != input.charAt(i)) {
+				System.out.println("Das ist kein Palindrom");
+				return;
+			}
+			else{
+				System.out.println("Das ist ein Palindrom");
+				break;
+			}
 		}
 		
 
